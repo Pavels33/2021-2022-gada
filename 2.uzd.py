34a3253f0240e966d452837fdf5673dfecfcd96f
Pavels33 <pavelsobolev871@gmail.com>
2021-09-21 15:05:19 +0300

    2,uzd

---
+++ 2.uzd.py
@@ -0,0 +1,15 @@
+#lietotais ievada divus veselos skaitlus a un b
+#izvadi visus skaitlus saja intervala.
+#input()
+#cikls
+#range
+
+a = int(input("Ievadi pirmo skaitli: "))
+b = int(input("Ievadi otro skaitli: "))
+
+for i  in range(a, b):
+    print(i)
+
+
+
+
