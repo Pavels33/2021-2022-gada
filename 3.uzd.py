7c56087e77172bc4428c05dbe3d575b7a4f81d40
Pavels33 <pavelsobolev871@gmail.com>
2021-09-21 15:20:59 +0300

    3.uzd

---
+++ 3.uzd.py
@@ -0,0 +1,12 @@
+#lietotajs ievada veselu pozitivu skautlu
+#izdruka visus skaitlus sakot no ievadita lidz 0
+
+#input
+#cikls
+
+n = int(input('ievadi veselu pozitivu skaitlu:'))
+
+while (n>0):
+    n -=1
+    print(n)
+
