34ef5588c31b466751bbc2733a6fd133e7e71879
Pavels33 <pavelsobolev871@gmail.com>
2021-09-21 14:40:14 +0300

    1,uzd

---
+++ 1 uzd.py
@@ -0,0 +1,15 @@
+#sula maksa 1.5 euro(bez pvn) siers 3 euro 3 euro(bez vpn)
+#Aprekini cik kopa ar pvn
+
+PVN = 0.21
+
+sulasCena = 1.5
+sulasPVN = sulasCena*PVN
+
+sieraCena = 3
+sieraPVN = sieraCena*PVN
+
+kopa = sulasPVN + sieraPVN + sieraCena + sulasCena
+
+print(kopa)
+
